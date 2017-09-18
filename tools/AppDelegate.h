5f88a6eabea7994c90753605f7a3616130e2c4ca
gaolele <gaolele@navinfo.com>
2017-09-18 15:01:27 +0800

    Initial Commit

---
+++ tools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tools
+//
+//  Created by navinfo on 2017/9/18.
+//  Copyright © 2017年 navinfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tools
+//
+//  Created by navinfo on 2017/9/18.
+//  Copyright © 2017年 navinfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
