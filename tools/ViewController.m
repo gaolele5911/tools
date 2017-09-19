15d89be544daeefd9fac4b701f62c4f5e5502fa5
gaolele <gaolele@navinfo.com>
2017-09-19 18:22:45 +0800

    添加 读写文件

--- tools/ViewController.m
+++ tools/ViewController.m
@@ -25,5 +25,43 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(NSData *)readFile {
+
+    //文件路径
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"kkmklm" ofType:nil];
+    NSData *reader=[NSData dataWithContentsOfFile:path];
+    
+    //读 0~100 字节数据
+    NSData *netPCMData = [reader subdataWithRange:NSMakeRange(0, 100)];
+
+    return netPCMData;
+}
+
+-(void)writefile:(NSData *)data
+{
+    NSArray *paths  = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES);
+    NSString *homePath = [paths objectAtIndex:0];
+    
+    NSString *filePath = [homePath stringByAppendingPathComponent:@"export-pcm.caf"];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    if(![fileManager fileExistsAtPath:filePath]) //如果不存在
+    {
+        [data writeToFile:filePath atomically:YES];
+    }
+    else//追加写入文件，而不是覆盖原来的文件
+    {
+        //        NSLog(@"-------文件存在，追加文件----------");
+        NSFileHandle *fileHandle = [NSFileHandle fileHandleForUpdatingAtPath:filePath];
+        
+        [fileHandle seekToEndOfFile];  //将节点跳到文件的末尾
+        
+        [fileHandle writeData:data]; //追加写入数据
+        
+        [fileHandle closeFile];
+    }
+}
+
 
 @end
